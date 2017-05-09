47f7514690bfd9d4f4d6c0d6cfedcdc2d0f36b42
weskitt <weskitt@hotmail.com>
2017-05-09 14:20:25 +0800

    5-9-14-20

--- WindowsFormsApp1/MemObject.cs
+++ WindowsFormsApp1/MemObject.cs
@@ -6,22 +6,49 @@ using System.Threading.Tasks;
 
 namespace WindowsFormsApp1
 {
-    class WordProperty
+    public class Pub
+    {
+        public const Char L_B = 'B';
+        public const Char L_C = 'C';
+        public const Char L_E = 'E';
+
+        public const int VERB = 0; //动词
+        public const int NOUN = 1; //名词
+        public const int MOD = 2; //修饰词
+
+        public const int V_NOUN = 3; //动-名
+        public const int V_MOD = 4; //动-修饰
+        public const int N_MOD = 5; //名-修饰
+
+        public const int VNM = 6; //动-名-修饰
+    }
+    class LinkProperty
     {
         char type; //链接类型，用于分词
-        int cout; //链接次数，用于分权
-        char fun; //链接功能，语法:Verb, Noun, ADJ, ADV
+        int cout=0; //链接次数，用于分权
+        int fun; //链接功能，语法:Verb, Noun, Mod
 
         public char Type { get => type; set => type = value; }
-        public int Cout { get => cout; set => cout = value; }
-        public char Fun { get => fun; set => fun = value; }
+        public int Cout { get => cout; set => cout += value; }
+        public int Fun { get => fun; set => fun = value; }
     }
-    class Word //字库
+    class Word //字
     {
         Char mWord; //主字
-        Dictionary<Char,int> subWords; //从字,以及链接次数
+        Dictionary<Char, LinkProperty> subWords; //从字,以及链接属性
 
-        public char MWord { get => mWord; set => mWord = value; }
+        public Boolean IfLink(char key)
+        {
+            return subWords.ContainsKey(key);
+        }
+
+        public void AddLink(char value, char type, int fun)
+        {
+            if (subWords.ContainsKey(value))
+            {
+                subWords[value].Cout = 1;
+            }
+        }
     }
     class Mem //记忆单元，暂时忽略声音影像
     {
